fdc406e1779c425292efe552d261ba619e55d640
Lisa Joanno <joanno.lisa@etu.unice.fr>
2016-02-12 08:17:19 +0100

    projet Maven

---
+++ testsMutations/src/main/java/devops4/testsMutations/App.java
@@ -0,0 +1,13 @@
+package devops4.testsMutations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
