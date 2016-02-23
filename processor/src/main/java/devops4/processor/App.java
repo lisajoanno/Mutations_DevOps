f83bfb263739ac3ef6fd0e4e2d8a8c23041bcf39
lisajoanno <lisa.joanno@gmail.com>
2016-02-23 15:27:33 +0100

    Maven project pour le processor ajouté

---
+++ processor/src/main/java/devops4/processor/App.java
@@ -0,0 +1,17 @@
+package devops4.processor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+//        	yep
+        } catch(Exception e) {
+        	
+        }
+    }
+}
