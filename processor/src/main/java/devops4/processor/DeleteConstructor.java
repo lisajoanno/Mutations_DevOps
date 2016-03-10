e0ff8a7a500ca0e331d7f49ff42f0cb53a26ece3
Arnaud-Garnier <arnaudgarnier06@gmail.com>
2016-03-10 16:25:58 +0100

    DeleteConstructor opé.

--- processor/src/main/java/devops4/processor/DeleteConstructor.java
+++ processor/src/main/java/devops4/processor/DeleteConstructor.java
@@ -1,27 +1,22 @@
 package devops4.processor;
 
+import java.util.Set;
+
 import spoon.processing.AbstractProcessor;
-import spoon.reflect.code.CtBlock;
-import spoon.reflect.code.CtCodeSnippetStatement;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtConstructor;
-import spoon.reflect.declaration.CtField;
-import spoon.reflect.declaration.ModifierKind;
 
+/**
+ * Processeur qui supprime les constructeurs d'une classe.
+ *
+ */
 public class DeleteConstructor extends AbstractProcessor<CtClass<?>> {
 
-	public void process(CtClass<?> ctClass) {
-
-		final CtField<String> s = getFactory().Core().<String> createField();
-
-		final CtCodeSnippetStatement statementInConstructor = getFactory().Code().createCodeSnippetStatement("");
-		final CtBlock<?> ctBlockOfConstructor = getFactory().Code().createCtBlock(statementInConstructor);
-		final CtConstructor constructor = getFactory().Core().createConstructor();
-		constructor.setBody(ctBlockOfConstructor);
-		constructor.addModifier(ModifierKind.PUBLIC);
-		// Apply transformation.
-		ctClass.addField(s);
-		ctClass.addConstructor(constructor);
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void process(CtClass candidate) {
+		Set<CtConstructor> construct = candidate.getConstructors();
+		for (CtConstructor c : construct) {
+			candidate.removeConstructor(c);
+		}
 	}
-
 }
